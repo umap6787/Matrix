235089fb40bacc20ff21da62b3b80feec97c3fb8
umap6787 <115221641+umap6787@users.noreply.github.com>
2023-10-06 17:39:10 +0530

    Add files via upload

---
+++ JavaProject/src/test/DuplicateElements.java
@@ -0,0 +1,16 @@
+package test;
+
+import java.util.Scanner;
+
+public class DuplicateElements {
+
+	public static void main(String[] args) {
+		
+		Scanner sc= new Scanner(System.in);
+		
+		
+		
+
+	}
+
+}
